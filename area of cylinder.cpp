662fdba51a191210c25923fa2e73a303d00c7dcb
PrakashsahGit <96606084+PrakashsahGit@users.noreply.github.com>
2021-12-28 15:34:08 +0545

    Add files via upload

---
+++ area of cylinder.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float volume,r,h;
+	cout<<"enter the radius and height of cylinder"<<endl;
+	cin>>r>>h;
+	volume=3.14*r*r*h;
+	cout<<"the area of cylinder is"<<volume;
+	return 0;
+	
+}
