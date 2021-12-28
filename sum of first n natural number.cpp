522211ae1c24785069d99ff10fe0ab47b807e5b9
PrakashsahGit <96606084+PrakashsahGit@users.noreply.github.com>
2021-12-28 15:36:26 +0545

    Add files via upload

---
+++ sum of first n natural number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int sum,n;
+	cout<<"enter the natural number"<<endl;
+	cin>>n;
+	sum=n*(n+1)/2;
+	cout<<"the sum of natural number is "<<sum;
+	return 0;
+	 
+}
