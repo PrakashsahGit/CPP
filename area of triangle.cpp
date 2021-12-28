a61e9f45f9e6465de7f75177a5b23ec8915e1edc
PrakashsahGit <96606084+PrakashsahGit@users.noreply.github.com>
2021-12-28 15:37:28 +0545

    Add files via upload

---
+++ area of triangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int b,h,area;
+	cout<<"Enter the base and height"<<endl;
+	cin>>b>>h;
+	area=(b*h)/2;
+	cout<<"The area of the tringle is "<<area;
+	return 0;
+	
+}
