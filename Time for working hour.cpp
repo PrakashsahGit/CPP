ef51cac9d7200c323730b1d76d58eeadaf68093c
PrakashsahGit <96606084+PrakashsahGit@users.noreply.github.com>
2021-12-29 23:41:22 +0545

    Add files via upload

---
+++ Time for working hour.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int time;
+	cout<<"Enter time"<<endl;
+	cin>>time;
+	if(time>=9 && time<=18){
+		cout<<"its working hour";
+	}
+	else{
+		cout<<"Leasure";
+	}
+	return 0;
+	
+	
+}
